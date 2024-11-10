7d02dbffe92f50ddf4803a99677ce16d90addc01
crab-apple <andreu.bastardas@gmail.com>
2024-11-10 18:30:47 +0100

    Extract method to get user session (automated refactoring, so allowed)

--- tripservice/java-1/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ tripservice/java-1/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,7 @@ public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         List<Trip> tripList = new ArrayList<Trip>();
-        User loggedUser = UserSession.getInstance().getLoggedUser();
+        User loggedUser = getUserSession().getLoggedUser();
         boolean isFriend = false;
         if (loggedUser != null) {
             for (User friend : user.getFriends()) {
@@ -29,4 +29,7 @@ public class TripService {
         }
     }
 
+    private UserSession getUserSession() {
+        return UserSession.getInstance();
+    }
 }
