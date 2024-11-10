15c3479c647d274ccd5055173fe5606a823791e9
crab-apple <andreu.bastardas@gmail.com>
2024-11-10 18:46:02 +0100

    Test throws exception when no user is logged in

--- tripservice/java-1/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ tripservice/java-1/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,30 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class TripServiceTest {
 
+    @Test
+    void ifNotLoggedIn_throwsNotLoggedInException() {
+
+        // Given
+        UserSession userSession = mock(UserSession.class);
+        when(userSession.getLoggedUser()).thenReturn(null);
+        TripService tripService = new TripService();
+        tripService.setInjectedSession(userSession);
+
+        // Then
+        assertThrows(
+                UserNotLoggedInException.class,
+                // When
+                () -> tripService.getTripsByUser(new User())
+        );
+    }
 }
