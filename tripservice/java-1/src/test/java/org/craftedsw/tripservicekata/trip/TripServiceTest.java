cb432cc90047ba0fc6aa0f4978a0bc6fc94c4acb
crab-apple <andreu.bastardas@gmail.com>
2024-11-10 21:48:03 +0100

    Add a couple more tests

--- tripservice/java-1/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ tripservice/java-1/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -5,7 +5,10 @@ import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,4 +44,38 @@ public class TripServiceTest {
                 () -> tripService.getTripsByUser(new User())
         );
     }
+
+    @Test
+    void ifUserIsNull_throwsNPE() {
+        // Given
+        User loggedUser = new User();
+        UserSession userSession = mock(UserSession.class);
+        when(userSession.getLoggedUser()).thenReturn(loggedUser);
+        TripService tripService = new TripService(userSession);
+
+        // Then
+        assertThrows(
+                NullPointerException.class,
+                // When
+                () -> tripService.getTripsByUser(null)
+        );
+    }
+
+    @Test
+    void ifUserIsNotFriendOfLoggedUser_returnsEmptyList() {
+        // Given
+        User loggedUser = new User();
+        User queriedUser = new User();
+        queriedUser.addTrip(new Trip());
+        UserSession userSession = mock(UserSession.class);
+        when(userSession.getLoggedUser()).thenReturn(loggedUser);
+        TripService tripService = new TripService(userSession);
+
+        // When
+        List<Trip> trips = tripService.getTripsByUser(queriedUser);
+
+        // Then
+        assertTrue(trips.isEmpty());
+    }
+
 }
